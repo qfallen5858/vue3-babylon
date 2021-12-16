f2cc330bbaddf9f98562d6dd9ee738348d1d1a1e
qufan <allen5858@163.com>
2021-12-16 19:29:52 +0800

    add babylon

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
+/*
+ * @Author: qufan
+ * @Date: 2021-12-16 15:35:44
+ * @LastEditTime: 2021-12-16 19:28:49
+ * @LastEditors: qufan
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \hello-vue3\src\main.js
+ */
+import { createApp} from 'vue'
+// import Vue from 'vue'
 import App from './App.vue'
+// import * as BABYLON from 'babylonjs'
+
+// Vue.prototype.BABYLON = BABYLON;
 
 createApp(App).mount('#app')
